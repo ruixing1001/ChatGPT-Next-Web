06a4ba5f85d2b424c1dadd624eada850d6650b67
Shao Ruixi <128834242+ruixing1001@users.noreply.github.com>
2023-04-23 00:56:58 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -124,7 +124,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={REPO_URL} target="_blank">
+            <a href={https://key.ruixing.fun} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>

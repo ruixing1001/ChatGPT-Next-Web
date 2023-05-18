00485e28668a980380f6272781d55e9921085d18
Shao Ruixi <128834242+ruixing1001@users.noreply.github.com>
2023-05-19 00:20:43 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -97,7 +97,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>小时的AI助手</div>
         <div className={styles["sidebar-sub-title"]}>
-          Xiaoshi AI assistant.
+          Xiaoshi AI assistant
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />

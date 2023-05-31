2b62fc790ad8017aa75023033d7242593adfabe8
Shao Ruixi <128834242+ruixing1001@users.noreply.github.com>
2023-05-31 17:19:34 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ import { getBuildConfig } from "./config/build";
 const buildConfig = getBuildConfig();
 
 export const metadata = {
-  title: "ChatGPT Next Web",
+  title: "王真的AI助手",
   description: "Your personal ChatGPT Chat Bot.",
   viewport: {
     width: "device-width",

bb2851c66c8e7109b7bf9ed39b34fdca69b59865
ruixing1001 <shaoruixing@gmail.com>
2024-08-03 13:15:28 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,7 +155,7 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          瑞星的AI助理
+          教育平台
         </div>
         <div className={styles["sidebar-sub-title"]}>
           为您提供全方位的帮助。
